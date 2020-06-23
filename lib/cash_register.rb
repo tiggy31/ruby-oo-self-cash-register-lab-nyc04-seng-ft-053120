0993454a81fcfd89cf58a8426af7cbdfd383fdd7
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-23 22:53:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
 
+class CashRegister
+  attr_reader :title
+  attr_accessor :total, :discount,:price
+  def initialize(discount= 0)
+    @total = 0
+    @discount= discount
+    @title
+    @price
+  end
+  
+  def add_item(title, price)
+    self.price +=1
+  end
+  
+end
